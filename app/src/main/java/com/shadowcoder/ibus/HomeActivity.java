a7daee5de297d916a6a6024538b0ff2877ff7593
syahmi92 <syahmi.imran92@gmail.com>
2021-07-09 14:52:10 +0800

    New commit from Syahmi
    New update!
    Redesign wave (with animation) P/s: Dark mode solve
    
    Add new design
    Add border to profile picture

--- app/src/main/java/com/shadowcoder/ibus/HomeActivity.java
+++ app/src/main/java/com/shadowcoder/ibus/HomeActivity.java
@@ -9,14 +9,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
 import com.google.android.material.navigation.NavigationView;
+import com.scwang.wave.MultiWaveHeader;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
